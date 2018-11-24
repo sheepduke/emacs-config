25e47a58dda9ceeec89024c218f076edb9d880c4
Danny YUE <sheepduke@gmail.com>
2018-11-25 00:29:38 +0800

    [base] Add sudo-find-file function

--- settings/a2-customize.el
+++ settings/a2-customize.el
@@ -150,7 +150,14 @@ output."
 ;; dired guesses the target directory
 (setq dired-dwim-target t)
 
+(defun sudo-find-file (file-name)
+  "Like find file, but opens the file as root."
+  (interactive "FSudo Find File: ")
+  (let ((tramp-file-name (concat "/sudo::" (expand-file-name file-name))))
+    (find-file tramp-file-name)))
+
 (define-key dired-mode-map (kbd ",") 'dired-kill-subdir)
+(global-set-key (kbd "C-x M-f") 'sudo-find-file)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                            Commands                              ;;
