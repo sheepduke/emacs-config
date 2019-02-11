077b9c6307641f19dde42841a94226e667224fd7
YUE Daian <yue_d@worksap.co.jp>
2019-02-11 16:11:53 +0800

    [session] Remove usage of elscreen-persist

--- settings/z3-session.el
+++ settings/z3-session.el
@@ -19,20 +19,3 @@
   	  (load-theme theme)))
   (add-hook 'desktop-after-read-hook 'desktop-load-theme)
   (add-to-list 'desktop-globals-to-save 'custom-enabled-themes))
-
-(use-package elscreen-persist
-  :ensure
-  :init
-  (defvar desktop-data-elscreen nil
-	"Variable to hold elscreen-persist data.")
-  (defun desktop-prepare-data-elscreen! ()
-	(setq desktop-data-elscreen
-		  (call-when-defined 'elscreen-persist-get-data)))
-  (defun desktop-evaluate-data-elscreen! ()
-	(when desktop-data-elscreen
-	  (call-when-defined 'elscreen-persist-set-data desktop-data-elscreen)))
-
-  :config
-  (add-to-list 'desktop-globals-to-save 'desktop-data-elscreen)
-  (add-hook 'desktop-after-read-hook 'desktop-evaluate-data-elscreen!)
-  (add-hook 'desktop-save-hook 'desktop-prepare-data-elscreen!))
