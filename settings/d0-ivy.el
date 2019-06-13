7c80d8718a53e1f109689f56a6bf34eaa91d5075
Danny YUE <sheepduke@gmail.com>
2019-06-13 22:39:17 +0800

    feat: add counsel-outline

---
+++ settings/d0-ivy.el
@@ -0,0 +1,12 @@
+;;; Package --- Settings for Ivy/Counsel as completion framework
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package counsel
+  :ensure
+  :bind
+  ("C-c C-j" . counsel-outline))
+
+;;; d0-ivy.el ends here
