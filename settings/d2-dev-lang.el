39c301c488bd43d25e6c37f35becd06cf79d9fb9
YUE Daian <sheepduke@gmail.com>
2020-05-31 00:49:18 +0800

    feat (dev): add ocp-indent to OCaml

--- settings/d2-dev-lang.el
+++ settings/d2-dev-lang.el
@@ -403,6 +403,21 @@
   :hook
   (tuareg-mode . merlin-mode))
 
+;; Consistent indentation.
+(use-package ocp-indent :ensure
+  :after tuareg
+
+  :preface
+  (defun ocp-indent-tuareg-buffer ()
+    "Indent buffer only when it is in tuareg mode."
+    (interactive)
+    (when (equal major-mode 'tuareg-mode)
+      (ocp-indent-buffer)))
+  
+  :hook
+  (tuareg-mode . ocp-setup-indent)
+  (before-save . ocp-indent-tuareg-buffer))
+
 ;;; Type tips.
 (use-package merlin-eldoc :ensure
   :after merlin
