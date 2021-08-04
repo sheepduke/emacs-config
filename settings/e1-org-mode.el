e3b48fdb7a903f6f02b6745c8b306b2f1892b7d0
Daian YUE <sheepduke@gmail.com>
2021-08-04 14:41:37 +0800

    feat (org): add hotkey

--- settings/e1-org-mode.el
+++ settings/e1-org-mode.el
@@ -75,6 +75,7 @@
 (define-key org-mode-map (kbd "C-c C-.") 'org-demote-subtree)
 (define-key org-mode-map (kbd "C-c C-l") 'org-toggle-link-display)
 (define-key org-mode-map (kbd "C-c C-i") 'org-mark-ring-goto)
+(define-key org-mode-map (kbd "C-<tab>") 'org-force-cycle-archived)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;                          Modules                             ;;;;
