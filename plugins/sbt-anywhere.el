a71746a365c9e2fbfe7ab92ed1a5f846aef77b27
YUE Daian <sheepduke@gmail.com>
2022-06-15 08:57:28 +0800

    feat(dev): add sbt mode

---
+++ plugins/sbt-anywhere.el
@@ -0,0 +1,41 @@
+(require 'sbt-mode)
+
+(defun sbt-anywhere-buffer-name ()
+  "Get sbt buffer name."
+  (sbt:buffer-name))
+
+(defun sbt-anywhere-send-string (string)
+  "Send given STRING to sbt console and execute it."
+  (with-current-buffer (sbt-anywhere-buffer-name)
+    (insert string)
+    (comint-send-input)))
+
+(defun sbt-anywhere-enter-repl ()
+  "Enter Scala REPL."
+  (interactive)
+  (sbt-anywhere-send-string "console"))
+
+(defun sbt-anywhere-leave-repl ()
+  "Leave Scala REPL."
+  (interactive)
+  (sbt-anywhere-send-string ":q"))
+
+(defun sbt-anywhere-send-line ()
+  "Send current line to sbt and execute it."
+  (interactive)
+  (sbt-anywhere-send-string (string-trim (thing-at-point 'line t))))
+
+(defun sbt-anywhere-send-region (start end)
+  "Send a region to REPL and evaluate it."
+  (interactive "r")
+  (let ((content (buffer-substring start end)))
+    (with-current-buffer (sbt-anywhere-buffer-name)
+      (insert content)
+      (comint-send-input))))
+
+(defun sbt-anywhere-send-buffer ()
+  "Send a buffer to REPL and evaluate it."
+  (interactive)
+  (sbt-anywhere-send-region (point-min) (point-max)))
+
+(provide 'sbt-anywhere)
--- settings/d2-dev-lang.el
+++ settings/d2-dev-lang.el
@@ -92,6 +92,24 @@
   (scala-mode . lsp)
   (scala-mode . lsp-metals-setup-hooks))
 
+(use-package sbt-mode)
+
+(use-package sbt-anywhere
+  :ensure nil
+  
+  :bind (:map scala-mode-map
+              ("C-c C-l" . sbt-anywhere-enter-repl)
+              ("C-c C-o" . sbt-anywhere-leave-repl)
+              ("C-c C-c" . sbt-anywhere-send-line)
+              ("C-c C-r" . sbt-anywhere-send-region)
+              ("C-c C-b" . sbt-anywhere-send-buffer)
+              ("C-c c c" . sbt-do-compile)
+              ("C-c c t" . sbt-do-test)
+              ("C-c c r" . sbt-do-run)
+              ("C-c c l" . sbt-do-clean)
+              ("C-c c p" . sbt-do-package)
+              ("C-M-l" . sbt-clear)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;                            Erlang                            ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
