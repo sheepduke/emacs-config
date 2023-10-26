45d134926d948d54d83ea975404d26150ce62ad6
YUE Daian <sheepduke@gmail.com>
2023-10-26 23:32:48 +0800

    feat(display): add nerd-icons-dired

---
+++ elpa/nerd-icons-dired-20230917.210/nerd-icons-dired-pkg.el
@@ -0,0 +1,15 @@
+(define-package "nerd-icons-dired" "20230917.210" "Shows icons for each file in dired mode"
+  '((emacs "24.4")
+    (nerd-icons "0.0.1"))
+  :commit "4a068884bf86647d242c3adc8320cd603e15dac3" :authors
+  '(("Hongyu Ding" . "rainstormstudio@yahoo.com"))
+  :maintainers
+  '(("Hongyu Ding" . "rainstormstudio@yahoo.com"))
+  :maintainer
+  '("Hongyu Ding" . "rainstormstudio@yahoo.com")
+  :keywords
+  '("lisp")
+  :url "https://github.com/rainstormstudio/nerd-icons-dired")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
