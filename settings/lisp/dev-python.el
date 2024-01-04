7c673d4d2aeee2616c93868ece0067d3082cae2c
YUE Daian <sheepduke@gmail.com>
2024-01-04 22:31:15 +0800

    feat: fix key bindings for python-ts-mode

--- settings/lisp/dev-python.el
+++ settings/lisp/dev-python.el
@@ -6,6 +6,6 @@
   (python-shell-interpreter "python3")
 
   :bind
-  (:map python-mode-map
+  (:map python-ts-mode-map
         ("C-c C-b" . python-shell-send-buffer)
         ("C-c C-c" . python-shell-send-statement)))
