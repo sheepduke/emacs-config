80a6910971fe24f0c3c5c323af25b190125e5898
YUE Daian <sheepduke@gmail.com>
2024-01-10 21:47:28 +0800

    fix(dev/haskell): minor changes

--- settings/lisp/dev-haskell.el
+++ settings/lisp/dev-haskell.el
@@ -1,8 +1,4 @@
 (use-package haskell-mode
-  :functions (haskell-session-interactive-buffer
-              haskell-interactive-handle-expr
-              haskell-interactive-copy-to-prompt)
-
   :preface
   (defun interactive-haskell-do-eval ()
     "Evaluate the current expression in Haskell REPL."
@@ -22,17 +18,16 @@
       (insert "r\n")
       (haskell-interactive-mode-return)))
 
-  (defun haskell-setup-hooks ()
+  (defun haskell-setup ()
     (interactive-haskell-mode 1)
     (let ((mode-map (symbol-value 'interactive-haskell-mode-map))
           (repl-mode-map (symbol-value 'haskell-interactive-mode-map)))
       (define-key mode-map (kbd "C-c C-c") 'interactive-haskell-eval-current-line)
       (define-key mode-map (kbd "C-c C-o") 'interactive-haskell-run-r)
-      (define-key repl-mode-map (kbd "C-c C-b") 'haskell-interactive-switch-back))
-    (highlight-indent-guides-mode 0))
+      (define-key repl-mode-map (kbd "C-c C-b") 'haskell-interactive-switch-back)))
 
   :init
   (require 'haskell-interactive-mode)
   
   :hook
-  (haskell-mode . haskell-setup-hooks))
+  (haskell-mode . haskell-setup))
