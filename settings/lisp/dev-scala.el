c268b03421c930b53b3514bb45e740b14c1436ca
YUE Daian <sheepduke@gmail.com>
2024-10-08 23:53:08 +0800

    feat(dev/scala): redefine hotkeys

--- settings/lisp/dev-scala.el
+++ settings/lisp/dev-scala.el
@@ -1,34 +1,35 @@
 (defun scala-smart-newline ()
   (interactive)
-  (let ((comment-symbol (and (line-in-comment?)
-                             (seq-first (string-trim-left (thing-at-point 'line))))))
+  (let ((comment-start (and (line-in-comment?)
+                            (substring (string-trim-left (thing-at-point 'line t)) 0 2))))
     (newline-and-indent)
-    (when comment-symbol
-      (insert (cl-case comment-symbol
-                (?* "* ")
-                (?/ "//")
-                (nil ""))))))
+    (when comment-start
+      (insert (cond
+               ((s-equals? comment-start "/*") "* ")
+               ((s-equals? comment-start "* ") "* ")
+               ((s-equals? comment-start "//") "// "))))))
 
 (defun scala-setup-buffer ()
   (eglot-ensure)
   
   (add-hook 'before-save-hook 'eglot-format-buffer nil t)
 
-  (local-set-key (kbd "C-c C-a") 'scala-repl-attach)
-  (local-set-key (kbd "C-c C-d") 'scala-repl-detach)
-  (local-set-key (kbd "C-c C-p") 'scala-repl-run)
-  (local-set-key (kbd "C-c C-s") 'scala-repl-restart)
-  (local-set-key (kbd "C-c C-l") 'scala-repl-save-and-load)
-  (local-set-key (kbd "C-c C-b") 'scala-repl-eval-buffer)
-  (local-set-key (kbd "C-c C-c") 'scala-repl-eval-region-or-line)
-  (local-set-key (kbd "C-c C-r") 'scala-repl-eval-main-function)
-  (local-set-key (kbd "C-M-l") 'scala-repl-clear)
-  (local-set-key (kbd "C-c C-v") 'compile-dwim)
-  (local-set-key (kbd "<return>" 'scala-smart-newline)))
+  ;; (local-set-key (kbd "C-c C-a") 'scala-repl-attach)
+  ;; (local-set-key (kbd "C-c C-d") 'scala-repl-detach)
+  ;; (local-set-key (kbd "C-c C-p") 'scala-repl-run)
+  ;; (local-set-key (kbd "C-c C-s") 'scala-repl-restart)
+  ;; (local-set-key (kbd "C-c C-l") 'scala-repl-save-and-load)
+  ;; (local-set-key (kbd "C-c C-b") 'scala-repl-eval-buffer)
+  ;; (local-set-key (kbd "C-c C-c") 'scala-repl-eval-region-or-line)
+  ;; (local-set-key (kbd "C-c C-r") 'scala-repl-eval-main-function)
+  ;; (local-set-key (kbd "C-M-l") 'scala-repl-clear)
+  ;; (local-set-key (kbd "C-c C-v") 'compile-dwim)
+
+  (local-set-key (kbd "<return>") 'scala-smart-newline))
 
 (use-package scala-ts-mode
   :mode "\\.scala\\'"
-  :mode "\\.sc\\'"
+  :mode "\\.CS\\'"
 
   :config
   (require 'eglot)
@@ -48,22 +49,31 @@
              scala-repl-eval-current-line
              scala-repl-eval-main-function))
 
-;; (use-package sbt-mode)
+(use-package sbt-mode)
 
-;; (use-package sbt-anywhere
-;;   :after (sbt scala-mode)
-  
-;;   :bind (:map scala-mode-map
-;;               ("C-c C-l" . sbt-anywhere-enter-repl)
-;;               ("C-c C-o" . sbt-anywhere-leave-repl)
-;;               ("C-c C-c" . sbt-anywhere-send-line)
-;;               ("C-c C-r" . sbt-anywhere-send-region)
-;;               ("C-c C-b" . sbt-anywhere-send-buffer)
-;;               ("C-c c c" . sbt-do-compile)
-;;               ("C-c c t" . sbt-do-test)
-;;               ("C-c c r" . sbt-do-run)
-;;               ("C-c c l" . sbt-do-clean)
-;;               ("C-c c p" . sbt-do-package)
-;;               ("C-c c s" . sbt-anywhere-do-restart)
-;;               ("C-c c a" . sbt-anywhere-do-assembly)
-;;               ("C-M-l" . sbt-clear)))
+(require 'sbt-plus)
+(use-package sbt-plus
+  :bind (:map scala-ts-mode-map
+              ("C-M-l" . sbt-clear)))
+
+(major-mode-hydra-define scala-ts-mode nil
+  ("SBT"
+   (("s" sbt-start "start")
+    ("S" sbt-plus-do-restart "restart"))
+
+   "REPL"
+   (("e" sbt-plus-enter-repl "enter")
+    ("E" sbt-plus-leave-repl "leave"))
+
+   "Send"
+   (("c" sbt-plus-send-line "line")
+    ("r" sbt-plus-send-region "region")
+    ("b" sbt-plus-send-buffer "buffer"))
+
+   "Action"
+   (("k" sbt-do-compile "compile")
+    ("t" sbt-do-run "run")
+    ("l" sbt-do-test "test")
+    ("C" sbt-do-clean "clean")
+    ("p" sbt-do-package "package")
+    ("a" sbt-plus-do-assembly))))
---
+++ settings/site-lisp/sbt-plus.el
@@ -0,0 +1,51 @@
+(require 'sbt-mode)
+
+(defun sbt-plus-buffer-name ()
+  "Get sbt buffer name."
+  (sbt:buffer-name))
+
+(defun sbt-plus-send-string (string)
+  "Send given STRING to sbt console and execute it."
+  (with-current-buffer (sbt-plus-buffer-name)
+    (insert string)
+    (comint-send-input)))
+
+(defun sbt-plus-enter-repl ()
+  "Enter Scala REPL."
+  (interactive)
+  (sbt-plus-send-string "console"))
+
+(defun sbt-plus-leave-repl ()
+  "Leave Scala REPL."
+  (interactive)
+  (sbt-plus-send-string ":q"))
+
+(defun sbt-plus-send-line ()
+  "Send current line to sbt and execute it."
+  (interactive)
+  (sbt-plus-send-string (string-trim (thing-at-point 'line t))))
+
+(defun sbt-plus-send-region (start end)
+  "Send a region to REPL and evaluate it."
+  (interactive "r")
+  (let ((content (buffer-substring start end)))
+    (with-current-buffer (sbt-plus-buffer-name)
+      (insert content)
+      (comint-send-input))))
+
+(defun sbt-plus-send-buffer ()
+  "Send a buffer to REPL and evaluate it."
+  (interactive)
+  (sbt-plus-send-region (point-min) (point-max)))
+
+(defun sbt-plus-do-restart ()
+  "Send reStart command from spray plugin."
+  (interactive)
+  (sbt-plus-send-string "reStart"))
+
+(defun sbt-plus-do-assembly ()
+  "Send assembly command from spray plugin."
+  (interactive)
+  (sbt-plus-send-string "assembly"))
+
+(provide 'sbt-plus)
